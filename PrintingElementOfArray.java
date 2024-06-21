a34a7d318a23f8da827925e1d9d528c35a9e90fd
Sakshee Upadhyay <upadhyaysakshee8@gmail.com>
2024-06-21 06:13:35 -0700

    Add files via upload

---
+++ PrintingElementOfArray.java
@@ -0,0 +1,10 @@
+package mylearning;
+
+public class PrintingElementOfArray {
+public static void main(String[] args) {
+	int[] arr = {2,3,4,5,6,7};
+	for(int i=0;i<arr.length;i++) {
+		System.out.print(arr[i]+" ");
+	}
+}
+}
